3460237e6561afecfc5da9045b066e678297eac0
Heechan Yang <92041942+Hellpython@users.noreply.github.com>
2026-06-26 17:14:05 +0900

    [Passed] 물의 상태 (75ms, 10MB)

---
+++ trail0/물의 상태/state-of-water.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+
+if (n >= -200 && n <= 200) {
+    if (n < 0) {
+    console.log('ice');    
+    } else if (n >= 100) {
+    console.log('vapor');
+    } else {
+    console.log('water');
+    }
+}
+
