64e019404907c921e039fe0a90b86b4a7412eeb0
Heechan Yang <92041942+Hellpython@users.noreply.github.com>
2026-07-07 19:15:09 +0900

    [Passed] 1부터의 합 (71ms, 10MB)

---
+++ trail0/1부터의 합/sum-from-the-one.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const N = Number(input);
+
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+
+  if (sum >= N) {
+    console.log(i);
+    break;
+  }
+}
