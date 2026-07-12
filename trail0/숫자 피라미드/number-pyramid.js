e234f4bf19c595516d72e8ed8e99c4064b53a0ff
Heechan Yang <92041942+Hellpython@users.noreply.github.com>
2026-07-12 23:47:31 +0900

    [Passed] 숫자 피라미드 (66ms, 10MB)

---
+++ trail0/숫자 피라미드/number-pyramid.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0).toString().trim();
+const N = Number(input);
+
+for (let i = 1; i <= N; i++) {
+  let line = "";
+
+  for (let j = 1; j <= i; j++) {
+    line += i + " ";
+  }
+
+  console.log(line.trim());
+}
