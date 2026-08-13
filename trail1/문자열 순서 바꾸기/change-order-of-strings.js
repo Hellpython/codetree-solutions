86879ae470f37173a4bf903c4493db1215484f43
Heechan Yang <92041942+Hellpython@users.noreply.github.com>
2026-08-13 11:04:27 +0900

    [Passed] 문자열 순서 바꾸기 (66ms, 10MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+let temp = input[0];
+
+let a = input[1];
+let b = temp;
+
+console.log(a);
+console.log(b);
